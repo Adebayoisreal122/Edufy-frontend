336f8233e0d165f9a203bbfa98f48caf88aaa802
adebayo isreal <adebayoisrealight122@gmail.com>
2024-04-18 11:50:25 +0100

    i worked on the signup pages

--- src/components/Adsignup.jsx
+++ src/components/Adsignup.jsx
@@ -1,33 +1,30 @@
 import React, { useState }  from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+
 
 const Adsignup = () => {
 
-  let URL = "http://localhost:3000/adminsignup";
-
-
-
+  let URL = "http://localhost:4000/register";
   const [firstName, setFirstName] = useState("");
   const [secondName, setSecondName] = useState("");
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const handleSignUp = () => {
-    axios.post(URL, { firstName, secondName, userName, email, password })
-      .then(response => {
-        // Handle successful sign-up, e.g., show a success message to the user
-        console.log(response.data.message);
-      })
-      .catch(error => {
-        // Handle sign-up error, e.g., show an error message to the user
-        console.error(error.response.data.message);
-      });
-  };
-
-
-
+  const Navigate = useNavigate();
+const handleSubmit = (e) => {
+  e.preventDefault();
+  axios.post(URL, {
+    firstName, secondName, userName, email, password
+  }
+  ).then((res) => {
+    console.log(res);
+  Navigate('/adminsignin');
+  }
+  ).catch((err) => {
+    console.log(err);
+  })}
 
 
   return (
@@ -36,35 +33,46 @@ const Adsignup = () => {
 <div className="container  my-5">
     <div className="row  justify-content-center">
         <div className=" col-lg-6">
-            <div className="position-relative border bg-dark border-info p-5 rounded rounded-4">
+            <div className="position-relative border bg-dark border-info p-3 rounded rounded-4">
                 <h1 className='text-center bg-dark text-light  mb-4'>Sign Up here</h1>
+                <center className='bg-dark'>
 
-                <form className='bg-dark'>
-                <div className="mb-3 bg-dark">
-                        <label htmlFor="exampleInputEmail1" className=" bg-dark text-light form-label" onChange={e => setEmail(e.target.value)} >Email address</label>
-                        <input type="email" className=" bg-dark form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className=" bg-dark  text-info form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className="mb-3 bg-dark">
-                        <label htmlFor="exampleInputEmail1" className=" bg-dark text-light form-label" onChange={e => setEmail(e.target.value)} >Email address</label>
-                        <input type="email" className=" bg-dark form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className=" bg-dark  text-info form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className=" bg-dark mb-3">
-                        <label htmlFor="exampleInputPassword1" className=" bg-dark form-label"  onChange={e => setPassword(e.target.value)}  >Password</label>
-                        <input type="password" className=" bg-dark form-control" id="exampleInputPassword1"/>
-                    </div>
-                   
+                <form  onSubmit={handleSubmit} className="form bg-dark">
+            <p className="title bg-dark">Register </p>
+            <p className="message bg-dark text-light">Signup now and get full access to our app. </p>
+                <div className=" d-flex bg-dark gap-3">
+                <label className='bg-dark'>
+                    <input name="firstName" required="" placeholder="" value={firstName} type="text" onChange={(e) => setFirstName (e.target.value)} className=" bg-dark input"/>
+                    <span className='bg-dark text-light'>Firstname</span>
+                </label>
+        
+                <label className='bg-dark'>
+                    <input name="secondName" required="" placeholder="" value={secondName} type="text" onChange={(e) => setSecondName (e.target.value)} className=" bg-dark input"/>
+                    <span className='bg-dark text-light'>Lastname</span>
+                </label>
+                
+            </div>  
+            
+            <label className='bg-dark'>
+                <input name="userName" required="" placeholder="" value={userName} type="text" onChange={(e) => setUserName (e.target.value)} className="bg-dark input"/>
+                <span className='bg-dark text-light'>Username</span>
+            </label> 
 
-                    <div className=" bg-dark form-check">
-                        <p className='bg-dark text-light'>You don't have an account? <Link to={"/"} className='bg-dark '>Sign up here</Link></p>
+            <label className='bg-dark'>
+                <input name="email" required="" placeholder="" value={email} type="email" onChange={(e) => setEmail (e.target.value)} className="bg-dark input"/>
+                <span className='bg-dark text-light'>Email</span>
+            </label> 
+                
+            <label className='bg-dark'>
+                <input name="password" required="" placeholder="" value={password} type="password" onChange={(e) => setPassword (e.target.value)} className="bg-dark input"/>
+                <span className='bg-dark text-light'>Password</span>
+            </label>
+            <button type="submit"  className="submit">Submit</button>
+            <div className=" bg-dark form-check">
+                        <p className='bg-dark text-light'>You don't have an account? <Link to={"/adminsignup"} className='bg-dark '>Sign up here</Link></p>
                     </div>
-               
-                    <div className='bg-dark'>
-
-                    <button type="submit" className="btn  btn-primary"  onClick={handleSignUp}  >Sign in</button>
-                    </div>
-                </form>
+        </form>
+                </center>
             </div>
         </div>
     </div>
--- src/components/Studentsignup.jsx
+++ src/components/Studentsignup.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Studentsignup = () => {
 
-  let URL = "http://localhost:3000/usersignup";
+  let URL = "http://localhost:4000/register";
   const [firstName, setFirstName] = useState("");
   const [secondName, setSecondName] = useState("");
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const handleSignUp = () => {
-    axios.post(URL, {firstName, secondName, userName, email, password })
-      .then(response => {
-        // Handle successful sign-up, e.g., show a success message to the user
-        console.log(response.data.message);
-      })
-      .catch(error => {
-        // Handle sign-up error, e.g., show an error message to the user
-        console.error(error.response.data.message);
-      });
-  };
+  const Navigate = useNavigate();
+const handleSubmit = (e) => {
+  e.preventDefault();
+  axios.post(URL, {
+    firstName, secondName, userName, email, password
+  }
+  ).then((res) => {
+    console.log(res);
+  Navigate('/adminsignin');
+  }
+  ).catch((err) => {
+    console.log(err);
+  })}
 
 
   return (
@@ -32,27 +34,44 @@ const Studentsignup = () => {
             <div className="position-relative border bg-dark border-info p-5 rounded rounded-4">
                 <h1 className='text-center bg-dark text-light  mb-4'>Sign Up here</h1>
 
-                <form className='bg-dark'>
-                    <div className="mb-3 bg-dark">
-                        <label htmlFor="exampleInputEmail1" className=" bg-dark text-light form-label" onChange={e => setEmail(e.target.value)} >Email address</label>
-                        <input type="email" className=" bg-dark form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className=" bg-dark  text-info form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div className=" bg-dark mb-3">
-                        <label htmlFor="exampleInputPassword1" className=" bg-dark form-label"  onChange={e => setPassword(e.target.value)}  >Password</label>
-                        <input type="password" className=" bg-dark form-control" id="exampleInputPassword1"/>
-                    </div>
-                   
+                <center className='bg-dark'>
 
-                    <div className=" bg-dark form-check">
-                        <p className='bg-dark text-light'>You don't have an account? <Link to={"/"} className='bg-dark '>Sign up here</Link></p>
-                    </div>
-               
-                    <div className='bg-dark'>
+<form  onSubmit={handleSubmit} className="form bg-dark">
+<p className="title bg-dark">Register </p>
+<p className="message bg-dark text-light">Signup now and get full access to our app. </p>
+<div className=" d-flex bg-dark gap-3">
+<label className='bg-dark'>
+    <input name="firstName" required="" placeholder="" value={firstName} type="text" onChange={(e) => setFirstName (e.target.value)} className=" bg-dark input"/>
+    <span className='bg-dark text-light'>Firstname</span>
+</label>
 
-                    <button type="submit" className="btn  btn-primary"  onClick={handleSignUp}  >Sign in</button>
-                    </div>
-                </form>
+<label className='bg-dark'>
+    <input name="secondName" required="" placeholder="" value={secondName} type="text" onChange={(e) => setSecondName (e.target.value)} className=" bg-dark input"/>
+    <span className='bg-dark text-light'>Lastname</span>
+</label>
+
+</div>  
+
+<label className='bg-dark'>
+<input name="userName" required="" placeholder="" value={userName} type="text" onChange={(e) => setUserName (e.target.value)} className="bg-dark input"/>
+<span className='bg-dark text-light'>Username</span>
+</label> 
+
+<label className='bg-dark'>
+<input name="email" required="" placeholder="" value={email} type="email" onChange={(e) => setEmail (e.target.value)} className="bg-dark input"/>
+<span className='bg-dark text-light'>Email</span>
+</label> 
+
+<label className='bg-dark'>
+<input name="password" required="" placeholder="" value={password} type="password" onChange={(e) => setPassword (e.target.value)} className="bg-dark input"/>
+<span className='bg-dark text-light'>Password</span>
+</label>
+<button type="submit"  className="submit">Submit</button>
+<div className=" bg-dark form-check">
+        <p className='bg-dark text-light'>You don't have an account? <Link to={"/adminsignup"} className='bg-dark '>Sign up here</Link></p>
+    </div>
+</form>
+</center>
             </div>
         </div>
     </div>
